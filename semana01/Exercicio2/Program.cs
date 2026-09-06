837c2337eda1c0065b2ceecad3f0d89986a46152
RuanFigueiredo <ruanthiago67@gmail.com>
2026-09-06 11:29:57 -0300

    Exercicio2. condicionais finalizado

--- semana01/Exercicio2/Program.cs
+++ semana01/Exercicio2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio2.");
+        Console.Write("Digite uma nota de 0 a 100: ");
+        int nota = int.Parse(Console.ReadLine());
+
+        String letra;
+
+        if(nota >=90 )
+            {
+                letra = "A";
+                      
+            }
+        else if(nota >=80)
+            {
+               letra = "B";
+                    
+            }
+        else if(nota >=70)
+            {
+                letra = "C";
+                      
+            }
+        else if(nota >=60)
+            {
+                letra = "D";
+                      
+            }
+        else
+            {
+                letra = "F";
+                  
+            }
+            
+            Console.WriteLine(letra);
+
+        if (nota >=70)
+            {
+                Console.WriteLine("Parabéns, você passou!");       
+            }
+        else
+            {
+                Console.WriteLine("Você não passou, não desista! tente novamente!");       
+            }
+        
+            
+            
+
     }
 }
