32e16631e7414c0d933c4f59b48d827ec2962274
RuanFigueiredo <ruanthiago67@gmail.com>
2026-09-05 23:55:11 -0300

    C# Preparação 1

--- semana01/Exercicio1/Program.cs
+++ semana01/Exercicio1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio1.");
+        Console.Write("Qual é o seu nome? ");
+       String nome = Console.ReadLine();
+
+       Console.Write("Qual é o seu sobrenome? ");
+       String sobrenome = Console.ReadLine();
+
+       Console.WriteLine($"Seu nome é {sobrenome}, {nome} {sobrenome}.");
     }
 }
