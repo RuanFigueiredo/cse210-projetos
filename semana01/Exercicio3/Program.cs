6e6e3f6f6002536db5809fb04cdd9a4e2b9c47ee
RuanFigueiredo <ruanthiago67@gmail.com>
2026-09-07 10:17:51 -0300

    semana 01 c# exercicio 3 concluido

--- semana01/Exercicio3/Program.cs
+++ semana01/Exercicio3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio3.");
+       
+        
+        // Solicita ao usuário que insira o número mágico e o palpite
+         Random geradorAleatorio = new Random();
+        int numeroMagico = geradorAleatorio.Next(1,101);
+
+     
+        int palpite = 0;
+
+        while (palpite != numeroMagico)
+        {
+            Console.Write("Qual é o seu palpite? ");
+            palpite = int.Parse(Console.ReadLine());
+
+            // condição para verificar se o palpite é maior, menor ou igual ao número mágico
+            if(palpite < numeroMagico)
+            {
+                Console.WriteLine("Mais alto");
+            }
+            else if(palpite > numeroMagico)
+            {
+                Console.WriteLine("Mais baixo");
+            }
+            else
+            {
+                Console.WriteLine("Você adivinhou!");
+            } 
+
+            }
+            
+        }
     }
-}
