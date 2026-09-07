eaeec85f82392634748e359216e32cca9c1afef6
RuanFigueiredo <ruanthiago67@gmail.com>
2026-09-07 16:27:32 -0300

    Semana01 c# Exercício4 finalizado

--- semana01/Exercicio4/Program.cs
+++ semana01/Exercicio4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio4.");
+         List<int> listaNumeros = new List<int>();
+
+        Console.WriteLine("Insira uma lista de números  e digite 0 quando terminar");
+
+        int numero= int.Parse(Console.ReadLine());
+
+
+        while(numero  != 0)
+        {
+          
+            listaNumeros.Add(numero);
+
+            numero = int.Parse(Console.ReadLine());
+                
+        }
+
+        int soma = 0;
+        int maiorNumero = listaNumeros[0];
+
+        foreach (int numeroLista in listaNumeros)
+        {
+            soma = soma + numeroLista;
+            if (maiorNumero < numeroLista)
+            {
+                maiorNumero = numeroLista;
+            }
+
+        }
+
+        double media = (double)soma / listaNumeros.Count;
+        Console.WriteLine("A soma é: " + soma);
+        Console.WriteLine("A média é: " + media);
+        Console.WriteLine("O maior número é: " + maiorNumero);
+
+
+
     }
 }
